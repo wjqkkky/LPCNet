8b271737648a1e2b7a055ee1022f139f715b603a
wangjiaqi12807 <wangjiaqi12807@autohome.com.cn>
2020-10-24 17:02:23 +0800

    set checkpoint period to 5

--- src/train_lpcnet.py
+++ src/train_lpcnet.py
@@ -99,11 +99,11 @@ del sig
 del pred
 
 # dump models to disk as we go
-checkpoint = ModelCheckpoint('lpcnet20_384_10_G16_{epoch:02d}.h5')
+checkpoint = ModelCheckpoint('lpcnet_niuman_{epoch:02d}.h5', period=5)
 
 # model.load_weights('lpcnet9b_384_10_G16_01.h5')
 model.compile(optimizer=Adam(0.001, amsgrad=True, decay=5e-5), loss='sparse_categorical_crossentropy')
 
 tb_callBack = TensorBoard(log_dir="./events", histogram_freq=0.5, write_grads=True)
 model.fit([in_data, in_exc, features, periods], out_exc, batch_size=batch_size, epochs=nb_epochs, validation_split=0.0,
-		  callbacks=[tb_callBack, checkpoint, lpcnet.Sparsify(2000, 40000, 400, (0.05, 0.05, 0.2))])
+		  callbacks=[checkpoint, lpcnet.Sparsify(2000, 40000, 400, (0.05, 0.05, 0.2))])
